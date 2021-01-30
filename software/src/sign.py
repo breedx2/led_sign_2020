a4bf1fa8ac51ca480744dc72248c9ca0cee3006a
jason <jason@noisybox.net>
2021-01-29 22:11:54 -0800

    move sign_state to simply sign and add invert and rot

--- software/src/sign.py
+++ software/src/sign.py
@@ -1,26 +1,49 @@
 
 COLS = 145
 
-class SignState:
-    """A low level representation of the sign state"""
+class Sign:
+    """A low level memory representation of the sign"""
 
     def __init__(self):
         # byte per column
         self.memory = [0x00] * COLS
 
     def col(self, index, value):
+        """sets an entire column value """
         self.memory[index] = value
+        return self
 
-    def on(self, row, col):
-        """turns on a bit at row,col"""
+    def on(self, col, row):
+        """turns on a bit at col,row"""
         self.memory[col] = self.memory[col] | (1<<row)
+        return self
 
-    def off(self, row, col):
+    def off(self, col, row):
         """turns off a bit at row,col"""
         self.memory[col] = self.memory[col] & ~(1<<row)
+        return self
 
     def clear(self):
         self.memory = [0x00] * COLS
+        return self
+
+    def invert(self):
+        self.memory = list(map(lambda x: x ^ 0xFF, self.memory))
+        return self
+
+    def rot_left(self):
+        self.memory.insert(0, self.memory.pop(len(self.memory)-1))
+        return self
+
+    def rot_right(self):
+        self.memory.append(self.memory.pop(0))
+        return self
+
+    def roll_down(self):
+        pass
+
+    def roll_up(self):
+        pass
 
     def print(self):
         for row in range(0,7):
