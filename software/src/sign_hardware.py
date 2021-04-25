86fd6be5c1d8eb781e152b9ec9b4c56cf4361f10
jason <jason@noisybox.net>
2021-04-25 10:51:31 -0700

    work in progress (getting spi going)

--- software/src/sign_hardware.py
+++ software/src/sign_hardware.py
@@ -1,10 +1,11 @@
-from machine import Pin
+from machine import Pin, SPI, SoftSPI, Timer
 import time
 
 # pin mappings
 EN    = const(21)
 CDATA = const(22)
 CCLK  = const(23)
+BS_IN = const(34)
 C1    = const(12)
 C2    = const(13)
 C3    = const(14)
@@ -15,13 +16,16 @@ C7    = const(18)
 
 COLS = const(145)
 
+ROWBUFF_LEN = const(19)
+rowbuff = bytearray(ROWBUFF_LEN)
+
 class SignHardware:
     """Sign hardware abstraction"""
 
     def __init__(self):
-        self.en    = Pin(EN,    Pin.OUT, value=0)
-        self.cdata = Pin(CDATA, Pin.OUT, value=0)
-        self.cclk  = Pin(CCLK,  Pin.OUT, value=0)
+        # self.spi = SoftSPI(baudrate=100000, polarity=1, phase=0, sck=Pin(CCLK), mosi=Pin(CDATA), miso=Pin(BS_IN))
+        self.spi = SPI(1, baudrate=3000000, polarity=1, phase=0, bits=8, firstbit=SPI.LSB, sck=Pin(CCLK), mosi=Pin(CDATA), miso=Pin(BS_IN))
+        self.en = Pin(EN, Pin.OUT, value=0)
         self.rows = [
             Pin(C1, Pin.OUT, value=0),
             Pin(C2, Pin.OUT, value=0),
@@ -48,17 +52,51 @@ class SignHardware:
         for row in self.rows:
             row.off()
 
-    def data_out(self, value):
-        self.cdata.value(0 if value == 0 else 1)
+    @micropython.native
+    def clear_rowbuff(self):
+        i = 0
+        while i < ROWBUFF_LEN:
+            rowbuff[i] = 0x00
+            i = i + 1
 
-    def clk_pulse(self):
-        self.cclk.on()
-        # time.sleep_us(1)
-        self.cclk.off()
+    @micropython.native
+    def shift_row(self, rownum, memory):
+        self.clear_rowbuff()
+        col = COLS - 1
+        mask = (1 << rownum)
+        while col >= 0:
+            bit_value = 1 if (memory[col] & mask) else 0
+            # jib = col + 3
+            i = int(col / 8)
+            if(bit_value):
+                bit_num = (col % 8) # firstbit LSB
+                rowbuff[i] = rowbuff[i] | (1 << bit_num)
+            # print("col = %d, mask = 0x%02x, bit_value = %d, i = %d, bit_num = %d, rowbuff[i] = 0x%02x" %(col, mask, bit_value, i, bit_num, rowbuff[i]))
+            col = col - 1
+
+        # print(rowbuff)
+        self.spi.write(rowbuff)
+
+    @micropython.native
+    def _compute_rowbuff(self, rownum, mem):
+        col = 0
+        srcmask = (1<<(6-rownum))
+        rbi = 0
+        while rbi < ROWBUFF_LEN:
+            rowbuff[rbi] = 0x00
+            rbi = rbi + 1
+        while col < COLS:
+            if(mem[col] & srcmask):
+                # rbi = int(col / 8)
+                rbi = ROWBUFF_LEN - 1 - int(col / 8)
+                rowbuff[rbi] = rowbuff[rbi] | (1<<(col % 8))
+                # rbi = ROWBUFF_LEN - 1 - int(col / 8)
+                # rowbuff[rbi] = rowbuff[rbi] | (1<<(col % 8))
+            col = col + 1
 
     # memory should be of size COLS
     @micropython.native
-    def shift_row(self, rownum, memory):
+    def xshift_row(self, rownum, memory):
         cdata = self.cdata
         cclk = self.cclk
         mask = (1<<rownum)
