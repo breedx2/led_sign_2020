983c63d8d502ee9597037edd90af682c55873d1c
jason <jason@noisybox.net>
2021-07-11 11:55:39 -0700

    add reverser

---
+++ software/firmware/main/sign_utils.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include "sign_utils.h"
+
+void reverse_buffer(uint8_t *buff, uint8_t length){
+  for(uint8_t i = 0; i < (length/2); i++){
+    uint8_t tmp = buff[i];
+    buff[i] = buff[length-i];
+    buff[length-i] = tmp;
+  }
+}
---
+++ software/firmware/main/sign_utils.h
@@ -0,0 +1,2 @@
+
+void reverse_buffer(uint8_t *buff, uint8_t length);
