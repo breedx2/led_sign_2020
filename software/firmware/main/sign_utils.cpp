2d8233f035758533ed1de7d7e0c9d373c2a1721b
jason <jason@noisybox.net>
2021-07-11 15:56:29 -0700

    add memory dump helper

--- software/firmware/main/sign_utils.cpp
+++ software/firmware/main/sign_utils.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "sign_memory.h"
 #include "sign_utils.h"
 
 void reverse_buffer(uint8_t *buff, uint8_t length){
@@ -6,5 +7,17 @@ void reverse_buffer(uint8_t *buff, uint8_t length){
     uint8_t tmp = buff[i];
     buff[i] = buff[length-i-1];
     buff[length-i-1] = tmp;
+  }
+}
+
+// Dumps the raw sign memory to the serial port
+void dump_mem_serial(){
+  for(uint8_t rownum = 0; rownum < 7; rownum++){
+    SIGN_ROW row = get_mem_row(rownum);
+    Serial.printf("%d: ", rownum);
+    for(uint8_t j = 0; j < BYTES_PER_ROW; j++){
+      Serial.printf("%02X ", row[j]);
+    }
+    Serial.printf("\r\n");
   }
 }
